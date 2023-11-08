0256a82797676b29e61d65edbb1d940ed493a662
Conor Thompson <conorthompson213@gmail.com>
2023-11-08 10:37:02 +0000

    added documentation

--- ookiebot/crawler/SiteMatcher.py
+++ ookiebot/crawler/SiteMatcher.py
@@ -1,24 +1,49 @@
 import sys
 import os
 
+# Add the parent directory to the sys.path so that you can import Logger
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from ..log.Logger import Logger
 
 class SiteMatcher:
+    """
+    SiteMatcher is a class for manipulating and matching website URLs.
+
+    Attributes:
+        threadId (str): A string representing the thread ID.
+        logger (Logger): An instance of the Logger class for logging.
+    """
     def __init__(self, threadId="main"):
+        """
+        Initialize a SiteMatcher object.
+
+        Args:
+            threadId (str): A string representing the thread ID (default is "main").
+        """
         self.threadId = threadId
         self.logger = Logger(threadId)
 
     def GetFullSiteURL(self, href, sourceSite):
+        """
+        Get the full URL of a site based on the provided href and source site.
+
+        Args:
+            href (str): The href to be matched.
+            sourceSite (str): The source site to use as a base URL.
+
+        Returns:
+            str: The full URL resulting from matching the href to the source site.
+        """
         if not href or not sourceSite or href[0] == "#":
             return sourceSite
         href = href.strip()
         siteDomain = sourceSite.strip()
         if siteDomain.count('/') >  2:
-            siteDomainArr = sourceSite.split("/",3)
+            siteDomainArr = sourceSite.split("/", 3)
             if siteDomainArr[0].lower() not in ["http:", "https:"]:
                 self.logger.debug("this is not a website -- "+siteDomainArr[0])
                 return
+
             siteDomain = siteDomainArr[0] + "//" + siteDomainArr[2].strip()
         
         # absolute url path (e.g. https://google.com/source/link, "/about/project" -> https://google.com/about/project)
@@ -36,4 +61,3 @@ class SiteMatcher:
             return sourceSite + "/" + href
 
         return href
-        
--- ookiebot/crawler/SiteProcessor.py
+++ ookiebot/crawler/SiteProcessor.py
@@ -5,36 +5,81 @@ import sys
 import pika
 from bs4 import BeautifulSoup
 
+# Import your local modules and dependencies here
 from ..crawler.SiteMatcher import SiteMatcher
 from ..crawler.Utils import Utils
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from ..log.Logger import Logger
 
+# Constants for RabbitMQ connection
 RABBITMQ_USERNAME = "pi"
 RABBITMQ_PASSWORD = "raspberry"
 
+# Timeouts for connection and reading
 conn_timeout = 6
 read_timeout = 20
 
+# Initialize RabbitMQ connection and channel
 connection = None
 channel = None
 
 def CreateAMQPConnection(conn):
+    """
+    Create and return an AMQP connection if it is closed or doesn't exist.
+
+    Args:
+        conn: An existing AMQP connection, if any.
+
+    Returns:
+        pika.BlockingConnection: The AMQP connection.
+    """
     if conn is None or conn.is_closed:
-        credentials = pika.PlainCredentials(RABBITMQ_USERNAME,RABBITMQ_PASSWORD)
-        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq.home.media',5672,'/', credentials))
+        credentials = pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
+        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq.home.media', 5672, '/', credentials))
         return connection
     return conn
 
 def CreateSiteChannel(conn):
+    """
+    Create a channel for the AMQP connection.
+
+    Args:
+        conn: An AMQP connection.
+
+    Returns:
+        pika.channel.Channel: The created channel.
+    """
     if conn.is_open:
         channel = conn.channel()
         channel.queue_declare(queue='sitesQueue')
         return channel
 
 class SiteProcessor:
-    def __init__(self, sourceSite, browser, visitedSites = {}, threadId="main"):
+    """
+    SiteProcessor is a class for processing and scraping websites.
+
+    Attributes:
+        visitedSites (dict): A dictionary of visited sites.
+        sourceSite (str): The source site URL.
+        sitesToVisit (list): A list of sites to visit.
+        threadId (str): A string representing the thread ID.
+        logger (Logger): An instance of the Logger class for logging.
+        siteMatcher (SiteMatcher): An instance of the SiteMatcher class.
+        amqpConnection (pika.BlockingConnection): An AMQP connection for message publishing.
+        channel (pika.channel.Channel): An AMQP channel for message publishing.
+        browser: The web browser object for web scraping.
+    """
+    def __init__(self, sourceSite, browser, visitedSites={}, threadId="main"):
+        """
+        Initialize a SiteProcessor object.
+
+        Args:
+            sourceSite (str): The source site to start processing.
+            browser: The web browser object for web scraping.
+            visitedSites (dict): A dictionary of visited sites (default is an empty dictionary).
+            threadId (str): A string representing the thread ID (default is "main").
+        """
         self.visitedSites = visitedSites
         self.sourceSite = sourceSite
         self.sitesToVisit = []
@@ -46,31 +91,37 @@ class SiteProcessor:
         self.browser = browser
 
     async def Process(self, _timeout=(6, 20)):
+        """
+        Process the source site, extract content, and push messages to RabbitMQ.
+
+        Args:
+            _timeout (tuple): A tuple containing connection and read timeouts (default is (6, 20)).
+        """
         try:
             page = await self.browser.newPage()
 
             # Navigate to the URL
             await page.goto(self.sourceSite)
 
-             # Wait for the page to load and execute JavaScript
+            # Wait for the page to load and execute JavaScript
             await page.waitForSelector('head')  # Replace with a suitable selector
 
             # Extract data from the rendered page
-            content = await page.evaluate('() => document.documentElement.innerHTML')  # Replace with your data extraction logic            
+            content = await page.evaluate('() => document.documentElement.innerHTML')  # Replace with your data extraction logic
             soup = BeautifulSoup(content, 'html.parser')
 
             self.logger.debug("closing page ["+self.sourceSite+"]")
             await page.close()
-            
+
             title = soup.find('title')
-            if title == None:
+            if title is None:
                 return None
-            
+
             title = title.string
             if "404" in title:
                 return None
 
-            # kill all script and style elements
+            # Kill all script and style elements
             for script in soup(["script", "style"]):
                 script.extract()
 
@@ -81,32 +132,55 @@ class SiteProcessor:
                 if word is None:
                     continue
                 words.extend(word.split())
-                
+
             for link in soup.find_all(href=True):
                 if link.has_attr('href'):
                     url = self.siteMatcher.GetFullSiteURL(link['href'], self.sourceSite)
                     if url is not None and url not in self.visitedSites and not Utils.isExcludedFileType(url):
                         self.sitesToVisit.append(url)
                         self.visitedSites[url] = True
-                        
+
             message = self.CreateSiteMessage(self.sourceSite, title, words)
             self.PushSiteMessage(message)
         except BaseException as ex:
             self.logger.error("Could not connect to site ["+self.sourceSite+"], Reason="+str(ex))
             raise ex
-            
+
     def FlushSitesToVisit(self):
+        """
+        Flush the list of sites to visit.
+
+        Returns:
+            list: The list of sites to visit.
+        """
         results = self.sitesToVisit
         self.sitesToVisit = []
         return results
 
     def CreateSiteMessage(self, url, title, words):
-        return json.dumps({"title": title, "url": url, "words":list(words)})
-    
+        """
+        Create a JSON message containing site information.
+
+        Args:
+            url (str): The site URL.
+            title (str): The site title.
+            words (list): A list of words from the site content.
+
+        Returns:
+            str: The JSON message.
+        """
+        return json.dumps({"title": title, "url": url, "words": list(words)})
+
     def PushSiteMessage(self, message):
+        """
+        Push a site message to RabbitMQ.
+
+        Args:
+            message (str): The message to push to the message queue.
+        """
         try:
             self.channel.basic_publish(exchange='',
-                          routing_key='sitesQueue',
-                          body=message)
+                routing_key='sitesQueue',
+                body=message)
         except:
             self.logger.error("AMQP Channel connection failed.")
